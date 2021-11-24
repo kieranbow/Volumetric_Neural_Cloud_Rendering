7dced3b05f98a8fd924d70aff9737a2c62392ec0
kieranbow <Kieran2.Bowsher@live.uwe.ac.uk>
2021-11-24 20:04:30 +0000

    Added folder structure and basic post-processing shader for raycasting

---
+++ Volumetric_Rendering/Assets/Script/RenderImage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderImage : MonoBehaviour
+{
+    [SerializeField] private Material material;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Matrix4x4 viewToWorld = Camera.main.cameraToWorldMatrix;
+        material.SetMatrix("_ViewToWorld", viewToWorld);
+        Graphics.Blit(source, destination, material);
+    }}
