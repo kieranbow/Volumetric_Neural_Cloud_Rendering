87c39ca84915508457dbf07b2e8dd2f488e919a1
kieranbow <Kieran2.Bowsher@live.uwe.ac.uk>
2022-02-06 13:40:50 +0000

    Added functionality for mesh loading. Have not tested it yet

--- PointsInsideUnitCube/PointsInsideUnitCube/Source.cpp
+++ PointsInsideUnitCube/PointsInsideUnitCube/Source.cpp
@@ -1,5 +1,6 @@
 // C++ includes
 #include <vector>
+#include <iostream>
 
 // Assimp
 #include <assimp\Importer.hpp>
@@ -25,44 +26,62 @@ struct Vertex
 };
 
 
-//bool LoadMeshData(std::vector<Vertex>& vertices, std::vector<unsigned short>& indices)
-//{
-//	for (unsigned int m = 0; m < pScene->mNumMeshes; m++)
-//	{
-//		pMesh = pScene->mMeshes[m];
-//
-//		for (unsigned int vrtIdx = 0; vrtIdx < pMesh->mNumVertices; vrtIdx++)
-//		{
-//			Vertex vertex;
-//
-//			vertex.position.x = pMesh->mVertices[vrtIdx].x;
-//			vertex.position.y = pMesh->mVertices[vrtIdx].y;
-//			vertex.position.z = pMesh->mVertices[vrtIdx].z;
-//
-//			vertices.push_back(vertex);
-//		}
-//
-//		for (unsigned int i = 0; i < pMesh->mNumFaces; i++)
-//		{
-//			aiFace* face = &pMesh->mFaces[i];
-//
-//			if (face->mNumIndices == 3)
-//			{
-//				indices.push_back(face->mIndices[0]);
-//				indices.push_back(face->mIndices[1]);
-//				indices.push_back(face->mIndices[2]);
-//			}
-//		}
-//	}
-//	return true;
-//}
+Assimp::Importer importer;
+const aiScene* pScene;
+const aiMesh* pMesh;
+
+std::vector<Vertex> vertices;
+std::vector<unsigned short> indices;
+
+bool LoadMeshData(std::vector<Vertex>& vertices, std::vector<unsigned short>& indices)
+{
+	for (unsigned int m = 0; m < pScene->mNumMeshes; m++)
+	{
+		pMesh = pScene->mMeshes[m];
+
+		for (unsigned int vrtIdx = 0; vrtIdx < pMesh->mNumVertices; vrtIdx++)
+		{
+			Vertex vertex;
+
+			vertex.position.x = pMesh->mVertices[vrtIdx].x;
+			vertex.position.y = pMesh->mVertices[vrtIdx].y;
+			vertex.position.z = pMesh->mVertices[vrtIdx].z;
+
+			vertices.push_back(vertex);
+		}
+
+		for (unsigned int i = 0; i < pMesh->mNumFaces; i++)
+		{
+			aiFace* face = &pMesh->mFaces[i];
+
+			if (face->mNumIndices == 3)
+			{
+				indices.push_back(face->mIndices[0]);
+				indices.push_back(face->mIndices[1]);
+				indices.push_back(face->mIndices[2]);
+			}
+		}
+	}
+	return true;
+}
 
 int main()
 {
 	// This file will generate a set of points within a unit cube
 
+	pScene = importer.ReadFile("Something here", aiProcess_JoinIdenticalVertices | aiProcess_Triangulate);
 
+	if (pScene == NULL)
+	{
+		std::cout << "Pointer to scene is null";
+		return 0;
+	}
 
+	if (!LoadMeshData(vertices, indices))
+	{
+		std::cout << "Mesh could not be loaded";
+		return 0;
+	}
 
 	return 0;
 }
