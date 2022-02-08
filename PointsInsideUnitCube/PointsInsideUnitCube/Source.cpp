20e828816be35c33d2f558645b95c24049c2e47c
kieranbow <Kieran2.Bowsher@live.uwe.ac.uk>
2022-02-08 20:39:58 +0000

    Updated ray-triangle intersection function

--- PointsInsideUnitCube/PointsInsideUnitCube/Source.cpp
+++ PointsInsideUnitCube/PointsInsideUnitCube/Source.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <iostream>
 #include <random>
+#include <functional>
 
 // Assimp
 #include <assimp\Importer.hpp>
@@ -12,6 +13,9 @@
 #include <assimp\postprocess.h>
 #include <assimp\matrix4x4.h>
 #include <assimp\cimport.h>
+
+// Defines
+constexpr float epsilon = 0.000001f;
 
 using Indices = unsigned short;
 
@@ -21,7 +25,6 @@ struct Vector3
 	Vector3() {}
 	Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
 
-	// Adds two vectors together
 	Vector3 operator+(const Vector3& a)
 	{
 		Vector3 vector3;
@@ -30,8 +33,6 @@ struct Vector3
 		vector3.z = this->z + a.z;
 		return vector3;
 	}
-
-	// Subtracts two vector
 	Vector3 operator-(const Vector3& a)
 	{
 		Vector3 vector3;
@@ -40,7 +41,6 @@ struct Vector3
 		vector3.z = this->z - a.z;
 		return vector3;
 	}
-
 	Vector3 operator*(const Vector3& a)
 	{
 		Vector3 vector3;
@@ -49,7 +49,6 @@ struct Vector3
 		vector3.z = this->z * a.z;
 		return vector3;
 	}
-
 	Vector3 operator/(const Vector3& a)
 	{
 		Vector3 vector3;
@@ -57,6 +56,18 @@ struct Vector3
 		vector3.y = this->y / a.y;
 		vector3.z = this->z / a.z;
 		return vector3;
+	}
+
+	static Vector3 cross(const Vector3& a, const Vector3& b)
+	{
+		float x = (a.y * b.z) - (a.z * b.y);
+		float y = (a.z * b.x) - (a.x * b.z);
+		float z = (a.x * b.y) - (a.y * b.x);
+		return Vector3(x, y, z);
+	}
+	static float dot(const Vector3& a, const Vector3& b)
+	{
+		return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
 	}
 
 	float x = 0.0f;
@@ -68,6 +79,7 @@ struct Vertex
 {
 	Vertex() {}
 	Vertex(float _x, float _y, float _z) : position(_x, _y, _z) {}
+
 	Vector3 position = { 0.0f, 0.0f, 0.0f };
 };
 
@@ -78,7 +90,8 @@ struct Ray
 
 	Vector3 origin = { 0.0f, 0.0f, 0.0f };
 	Vector3 direction = { 0.0f, 0.0f, 0.0f };
-	float t = 0; // Ray origin to point
+	float t = 0.0f; // Ray origin to point
+	int hit = 0;
 };
 
 Assimp::Importer importer;
@@ -104,15 +117,6 @@ Vector3 cross_product(Vector3 a, Vector3 b)
 	float y = (a.z * b.x) - (a.x * b.z);
 	float z = (a.x * b.y) - (a.y * b.x);
 	return Vector3(x, y, z);
-}
-
-Vector3 sub(Vector3 v1, Vector3 v2)
-{
-	Vector3 dest;
-	dest.x = v1.x - v2.x;
-	dest.y = v1.y - v2.y;
-	dest.z = v1.z - v2.z;
-	return dest;
 }
 
 bool LoadMeshData(std::vector<Vertex>& _vertices, std::vector<Indices>& _indices)
@@ -147,111 +151,79 @@ bool LoadMeshData(std::vector<Vertex>& _vertices, std::vector<Indices>& _indices
 	return true;
 }
 
+// Moller & Trumbore Ray-triangle intersection
 // https://cadxfem.org/inf/Fast%20MinimumStorage%20RayTriangle%20Intersection.pdf
-bool intersect_triangle(Ray ray, Vector3 vert0, Vector3 vert1, Vector3 vert2, float t, float u, float v, bool enable_cull)
+bool intersect_triangle(Ray ray, Vector3 vert0, Vector3 vert1, Vector3 vert2, float& t, float& u, float& v)
 {
-	float epsilon = 0.000001f;
-	Vector3 edge1;
-	Vector3 edge2;
-	Vector3 tvec;
-	Vector3 pvec;
-	Vector3 qvec;
-	float determinant;
-	float inv_determinant;
+	// https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/moller-trumbore-ray-triangle-intersection
 
-	// Find vectors for two edges that share vert0
-	edge1 = sub(vert1, vert0);
-	edge2 = sub(vert2, vert0);
+	// Find the two edge vectors
+	Vector3 v0v1 = vert1 - vert0;
+	Vector3 v0v2 = vert2 - vert0;
 
 	// Begin calculating determinant - also used to calculate U parameter
-	pvec = cross_product(ray.direction, edge2);
+	Vector3 pvec = Vector3::cross(ray.direction, v0v2);
 
-	// if determinant is near zero, ray lies in plane of triangle
-	determinant = dot_product(edge1, pvec);
+	float det = Vector3::dot(pvec, v0v1); //dot_product(pvec, v0v1)
 
-	if (enable_cull)
-	{
-		// Test double sided triangle
-		if (determinant < epsilon) return false;
+	// if the determinant is negative the triangle is backfacing
+	// if the determinant is close to 0, the ray misses the triangle
+	//if (det < EPSILON) return false; // Culls backface triangles
 
-		// Calculate distance from vert0 to ray origin
-		tvec = sub(ray.origin, vert0);
+	if (fabs(det) < epsilon) return false; // Allows Backface triangles
 
-		// Calculate U parameter and test bounds
-		u = dot_product(tvec, pvec);
-		if (u < 0.0f || u > determinant) return false;
+	float invDet = 1.0f / det;
 
-		// Prepare to test v parameter
-		qvec = cross_product(tvec, edge1);
+	// Calculate distance from vert0 to ray's origin
+	Vector3 tvec = ray.origin - vert0;
 
-		// Calculate V parameter and test bounds
-		v = dot_product(ray.origin, qvec);
-		if (v < 0.0f || u + v > determinant) return false; // u + v cannot be greater than 1
+	// Calculate U parameter and test bounds
+	u = dot_product(tvec, pvec) * invDet;
+	if (u < 0.0f || u > 1.0f) return false;
 
-		// Calculate t, scale parameters, ray intersects triangle
-		t = dot_product(edge2, qvec);
-		inv_determinant = 1.0f / determinant;
+	// Prepare to test V parameter
+	Vector3 qvec = cross_product(tvec, v0v1);
 
-		t *= inv_determinant;
-		u *= inv_determinant;
-		v *= inv_determinant;
+	// Calculate V parameter and test bounds
+	v = dot_product(ray.direction, qvec) * invDet;
+	if (v < 0.0f || u + v > 1.0f) return false; // u + v should never be greater than 1
 
-	}
-	if (!enable_cull)
-	{
-		if (determinant > -epsilon && determinant < epsilon) return false;
-
-		inv_determinant = 1.0f / determinant;
-
-		// Calculate distance from vert0 to ray origin
-		tvec = sub(ray.origin, vert0);
-
-		// Calculate U parameter and test bounds
-		u = dot_product(tvec, pvec) * inv_determinant;
-		if (u < 0.0f || u > 1.0f) return false;
-
-		// Prepare to test v parameter
-		qvec = cross_product(tvec, edge1);
-
-		// Calculate V parameter and test bounds
-		v = dot_product(ray.origin, qvec) * inv_determinant;
-		if (v < 0.0f || u + v > 1.0f) return false; // u + v cannot be greater than 1
-
-		// Calculate t, scale parameters, ray intersects triangle
-		t = dot_product(edge2, qvec) * inv_determinant;
-	}
+	// Calculate t, ray intersection triangle
+	t = dot_product(v0v2, qvec) * invDet;
 
 	return true;
 }
 
-// Generate a number from min and max
-int uniformDistribution(int min, int max)
+// Generates a floating point number from min and max
+float uniform_real_distribution(float min, float max)
 {
-	std::default_random_engine random_engine(static_cast<unsigned int>(std::random_device{}()));
-	std::uniform_int_distribution<> dis(min, max);
-	int gen_number = dis(random_engine);
-	return gen_number;
+	std::mt19937 generator(std::random_device{}());
+	std::uniform_real_distribution<float> dis(min, max);
+	auto number = dis(generator);
+	return number;
 }
 
-// Generate a position using uniform Distribution between min and max
-Vector3 generate_random_position(int min, int max)
+// Generate a random position between min and max
+Vector3 generate_random_position(float min, float max)
 {
 	Vector3 position;
-	position.x = static_cast<float>(uniformDistribution(min, max));
-	position.y = static_cast<float>(uniformDistribution(min, max));
-	position.z = static_cast<float>(uniformDistribution(min, max));
+	position.x = uniform_real_distribution(min, max);
+	position.y = uniform_real_distribution(min, max);
+	position.z = uniform_real_distribution(min, max);
 	return position;
 }
 
-// Generate a direction using uniform Distribution between min and max
-Vector3 generate_random_direction(int min, int max)
+// Generate a random direction between min and max
+Vector3 generate_random_direction(float min, float max)
 {
 	Vector3 direction;
-	direction.x = static_cast<float>(uniformDistribution(min, max));
-	direction.y = static_cast<float>(uniformDistribution(min, max));
-	direction.z = static_cast<float>(uniformDistribution(min, max));
+	direction.x = uniform_real_distribution(min, max);
+	direction.y = uniform_real_distribution(min, max);
+	direction.z = uniform_real_distribution(min, max);
 	return direction;
 }
+
+
 
 int main()
 {
@@ -312,20 +284,18 @@ int main()
 		//}
 	}
 
-	Ray test_ray;
-	test_ray.origin = { -2.0f, -0.25f, 0.45f };
-	test_ray.direction = { 1.0f, 0.0f, 0.0f };
+	Ray x_ray;
+	x_ray.origin = { -2.0f, -0.25f, 0.45f };
+	x_ray.direction = { 1.0f, 0.0f, 0.0f };
 
-	Vector3 world_origin(0.0f, 0.0f, 0.0f);
-	
 	std::vector<Ray> rays;
 
 	// Generate a set of rays with different origins and directions
 	for (int i = 0; i < 10; i++)
 	{
 		Ray ray;
-		ray.origin = generate_random_position(-1, 5);
-		ray.direction = generate_random_direction(-1, 5) - world_origin;
+		ray.origin = {-2.0f, uniform_real_distribution(-0.5f, 0.5f), uniform_real_distribution(-0.5f, 0.5f) };
+		ray.direction = { 0.0f, 0.0f, 0.0f };
 		rays.push_back(ray);
 	}
 
@@ -349,42 +319,58 @@ int main()
 		
 		int idx = (i / 3);
 
-		// Loop through all rays and check if any hit the triangle.
-		for (int j = 0; j < rays.size(); j++)
+		//// Loop through all rays and check if any hit the triangle.
+		//for (int j = 0; j < rays.size(); j++)
+		//{
+		//	if (intersect_triangle(rays.at(j), vert0, vert1, vert2, t, u, v))
+		//	{
+		//		float uv = u + v;
+
+		//		std::cout << "Ray[" + std::to_string(j) + "] hit Triangle" + "\t [" + std::to_string(idx) + "] \t uv: " + std::to_string(uv) + "\n";
+		//		hit++;
+		//		rays.at(j).hit += 1;
+		//	}
+		//	else
+		//	{
+		//		std::cout << "Ray[" + std::to_string(j) + "] missed Triangle" +  "\t [" + std::to_string(idx) + "]\n";
+		//	}
+		//}
+		//std::cout << "-------------------------------------\n";
+
+		if (intersect_triangle(x_ray, vert0, vert1, vert2, t, u, v))
 		{
-			if (intersect_triangle(rays.at(j), vert0, vert1, vert2, t, u, v, false))
-			{
-				std::cout << "Ray[" + std::to_string(j) + "] hit Triangle" + "\t [" + std::to_string(idx) + "]\n";
-				hit++;
-			}
-			else
-			{
-				std::cout << "Ray[" + std::to_string(j) + "] missed Triangle" +  "\t [" + std::to_string(idx) + "]\n";
-			}
+			std::cout << "Hit Triangle [" + std::to_string(idx) + "]\n";
+
+			//std::string vertex_1_pos = std::to_string(vert0.x) + ", \t" + std::to_string(vert0.y) + ", \t" + std::to_string(vert0.z);
+			//std::string vertex_2_pos = std::to_string(vert1.x) + ", \t" + std::to_string(vert1.y) + ", \t" + std::to_string(vert1.z);
+			//std::string vertex_3_pos = std::to_string(vert2.x) + ", \t" + std::to_string(vert2.y) + ", \t" + std::to_string(vert2.z);
+
+			//std::cout << "Vertex 1: " + vertex_1_pos + "\n";
+			//std::cout << "Vertex 2: " + vertex_2_pos + "\n";
+			//std::cout << "Vertex 3: " + vertex_3_pos + "\n";
+
 		}
-
-		std::cout << "-------------------------------------\n";
-
-		//if (intersect_triangle(test_ray, vert0, vert1, vert2, t, u, v, false))
-		//{
-		//	std::cout << "Ray [] Hit Triangle [" + std::to_string(idx) + "]\n";
-
-		//	//std::string vertex_1_pos = std::to_string(vert0.x) + ", \t" + std::to_string(vert0.y) + ", \t" + std::to_string(vert0.z);
-		//	//std::string vertex_2_pos = std::to_string(vert1.x) + ", \t" + std::to_string(vert1.y) + ", \t" + std::to_string(vert1.z);
-		//	//std::string vertex_3_pos = std::to_string(vert2.x) + ", \t" + std::to_string(vert2.y) + ", \t" + std::to_string(vert2.z);
-
-		//	//std::cout << "Vertex 1: " + vertex_1_pos + "\n";
-		//	//std::cout << "Vertex 2: " + vertex_2_pos + "\n";
-		//	//std::cout << "Vertex 3: " + vertex_3_pos + "\n";
-
-		//}
-		//else
-		//{
-		//	std::cout << "Miss Triangle [" + std::to_string(idx) + "]\n";
-		//}
+		else
+		{
+			std::cout << "Miss Triangle [" + std::to_string(idx) + "]\n";
+		}
 	}
 
-	std::cout << std::to_string(hit) + "/" + std::to_string(total) + "\n";
+	//for (auto& ray : rays)
+	//{
+	//	if (ray.hit % 2)
+	//	{
+	//		std::cout << "water tight mesh\n";
+	//	}
+	//	else
+	//	{
+	//		std::cout << "not water tight mesh\n";
+	//	}
 
-	return 0;
+	//	std::cout << std::to_string(ray.hit) + "\n";
+	//}
+
+	//std::cout << std::to_string(hit) + "/" + std::to_string(total) + "\n";
+
+	return 1;
 }
