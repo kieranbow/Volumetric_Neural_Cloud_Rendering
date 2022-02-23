27f736247f36854512cd11b2322bbdc121cf3941
kieranbow <Kieran2.Bowsher@live.uwe.ac.uk>
2022-02-23 15:25:52 +0000

    Added mlp.py file

---
+++ Neural Network/mlp.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 15:23:42 2022
+
+@author: Kiera
+"""
+
--- PointsInsideUnitCube/PointsInsideUnitCube/Source.cpp
+++ PointsInsideUnitCube/PointsInsideUnitCube/Source.cpp
@@ -357,7 +357,7 @@ int main()
 	{
 		Ray ray;
 		ray.origin = point.position;
-		ray.direction = Vector3(1.0f, 0.0f, 0.0f);
+		ray.direction = Vector3(1.0f, 0.0f, 0.0f) - point.position;
 
 		// Loop through mesh triangles and check if ray collided
 		for (int i = 0; i < indices.size(); i += 3)
